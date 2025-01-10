bdbd6bc0d3c77ec19d736e6dd6678564819905f7
st-dev <stdev.net@gmail.com>
2025-01-10 20:40:29 +0530

    Initial commits

---
+++ otoi.web/src/pages/parties/PartiesPersonsContent.tsx
@@ -0,0 +1,11 @@
+import { Persons } from "./blocks/persons";
+
+const PartiesPersonContent = () => {
+  return (
+    <div className="grid gap-5 lg:gap-7.5">
+      <Persons />
+    </div>
+  );
+};
+
+export { PartiesPersonContent };
